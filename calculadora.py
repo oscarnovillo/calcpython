957dae4a8853f7b327886e06351284bd0fe95569
oscarnovillo <oscar.novillo@gmail.com>
2025-04-01 16:42:29 +0200

    sumar

--- calculadora.py
+++ calculadora.py
@@ -10,6 +10,11 @@ def main():
         print("5. Salir")
         opcion = input("Elija una opción: ")
         print("opcion",opcion)
+        if opcion == "1":
+            numero1 = float(input("Ingrese el primer número: "))
+            numero2 = float(input("Ingrese el segundo número: "))
+            resultado = numero1 + numero2
+            print("El resultado de la suma es: ", resultado)
         seguir = opcion != "5"
 
     
