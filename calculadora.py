1835dabfbe24b11047b599d4b0f031ee9bd75186
oscarnovillo <oscar.novillo@gmail.com>
2025-04-16 20:08:28 +0200

    test hook

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,13 @@
-#Calculadora de operaciones básicas
+# import os  # Import no utilizado
+
+
+# Calculadora de operaciones básicas
 def main():
     seguir = True
     while seguir:
         print("Calculadora para github otro cambio")
         print("1. S ")
+        print("")
         print("2. Restar")
         print("3. Multiplicar")
         print("4. Dividir")
@@ -11,16 +15,16 @@ def main():
         print("4. D")
         print("5. Salir")
         opcion = input("Elija una opción: ")
-        print("opcion",opcion)
+        print("opcion", opcion)
         if opcion == "1":
             numero1 = float(input("primer num: "))
             numero2 = float(input("Ingrese el segundo número: "))
             resultado = numero1 + numero2
-            print("El resultado de la suma es: ", resultado)
+            print(
+                "El resultado de la suma es: ", resultado
+            )  # Espacios extra al final ->
         seguir = opcion != "5"
 
-    
-    
+
 if __name__ == "__main__":
     main()
-    
--- nuevo.py
+++ nuevo.py
@@ -1,5 +1,5 @@
 # nuevo
 
-#funcionalidad nueva
+# funcionalidad nueva
 
-#asdasd
+# asdasd
